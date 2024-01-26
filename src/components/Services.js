a55d848865feb3fd9e40eb3530ba5a371953806f
Hamza Haseeb <haseebcodejourney@gmail.com>
2024-01-26 05:53:33 +0500

    add component folder and add some dummy component files

---
+++ src/components/Services.js
@@ -0,0 +1,12 @@
+// src/components/Project.js
+import React from 'react';
+
+const Services = ({ title, description, technologies }) => {
+  return (
+    <div>
+      <h2>Services</h2>
+    </div>
+  );
+};
+
+export default Services;
